3ce948b00681481172aa9cc1f337404d5f52d022
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-05-24 10:47:13 -0400

    Preparando app para despliegue y habilitando Cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -28,6 +28,13 @@ import { CategoriasModule } from './categorias/categorias.module';
     ConfigModule.forRoot({isGlobal:true}),
 
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+              ? { rejectUnauthorized: false }
+              : null,
+      },
+
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
