e1a68c61f232c6d28d3785aa28bb97e6a0410e41
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-08-12 12:07:22 -0400

    categoria module add in apoyogubernamental modul

--- src/apoyosgubernamentales/apoyosgubernamentales.module.ts
+++ src/apoyosgubernamentales/apoyosgubernamentales.module.ts
@@ -4,11 +4,15 @@ import { ApoyosgubernamentalesController } from './apoyosgubernamentales.control
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Apoyosgubernamentale } from './entities/apoyosgubernamentale.entity';
 import { Unidadeseducativa } from 'src/unidadeseducativas/entities';
+import { CategoriasService } from 'src/categorias/categorias.service';
+import { CategoriasModule } from 'src/categorias/categorias.module';
 
 @Module({
   controllers: [ApoyosgubernamentalesController],
   providers: [ApoyosgubernamentalesService],
-  imports: [TypeOrmModule.forFeature([Apoyosgubernamentale, Unidadeseducativa ])],
+  imports: [TypeOrmModule.forFeature([Apoyosgubernamentale, Unidadeseducativa ]),  
+    CategoriasModule
+  ],
   exports: [ApoyosgubernamentalesService]
 })
 export class ApoyosgubernamentalesModule {}
